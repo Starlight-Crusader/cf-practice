0141dde8ef6e075ffbd7a3b4396a6a9b825535ac
AKalamaghin <arteom.kalamaghin@isa.utm.md>
2024-06-22 19:25:32 +0300

    1843D solution draft 2 (time)

--- D/1843/main.cpp
+++ D/1843/main.cpp
@@ -2,57 +2,12 @@
 #include <vector>
 #include <queue>
 
-int countSinks(std::vector<std::vector<int>> al, std::vector<int> d, int start)
-{
-    std::queue<int> tq; tq.push(start);
-    std::vector<bool> visited(al.size(), false);
-    std::vector<int> sinks;
-
-    while (!tq.empty())
-    {
-        int cur = tq.front(); tq.pop();
-
-        if (visited[cur])
-        {
-            continue;
-        } 
-        else
-        {
-            visited[cur] = true;
-        }
-
-        std::vector<int> itq;
-        
-        for (int v : al[cur])
-        {
-            if (!visited[v] && d[cur] < d[v])
-            {
-                itq.push_back(v);
-            }
-        }
-
-        if (itq.empty())
-        {
-            sinks.push_back(cur);
-        }
-        else
-        {
-            for (int v : itq)
-            {
-                tq.push(v);
-            }
-        }
-    }
-
-    return sinks.size();
-}
-
-std::vector<int> calcDepths(std::vector<std::vector<int>> al)
+std::vector<int> calcLayers(std::vector<std::vector<int>> al)
 {
     std::queue<int> tq; tq.push(0);
     std::vector<bool> visited(al.size(), false);
 
-    std::vector<int> depths(al.size(), 0);
+    std::vector<int> layers(al.size(), 0);
     
     while(!tq.empty())
     {
@@ -71,13 +26,104 @@ std::vector<int> calcDepths(std::vector<std::vector<int>> al)
         {
             if (!visited[v])
             {
-                depths[v] = depths[cur] + 1;
+                layers[v] = layers[cur] + 1;
                 tq.push(v);
             }
         }
     }
 
-    return depths;
+    return layers;
+}
+
+std::vector<std::vector<int>> compileLMap(int ml, std::vector<int> ll, std::vector<std::vector<int>> al) 
+{
+    std::vector<std::vector<int>> lm(ml + 1);
+
+    for (unsigned long int i = 0; i < al.size(); i++)
+    {
+        lm[ll[i]].push_back(i);
+    }
+
+    return lm;
+}
+
+std::vector<std::vector<int>> lookupSrcs(int maxLayer, std::vector<int> layerList, std::vector<std::vector<int>> tree, std::vector<std::vector<int>> layerMap)
+{
+    std::vector<std::vector<int>> srcs(tree.size());
+
+    for (int i = 1; i < maxLayer + 1; i++)
+    {
+        for (int v : layerMap[i])
+        {
+            for (int u : tree[v])
+            {
+                if (layerList[u] < layerList[v])
+                {
+                    srcs[v].push_back(u);
+
+                    for (int y : srcs[u])
+                    {
+                        srcs[v].push_back(y);
+                    }
+                }
+            }
+        }
+    }
+
+    return srcs;
+}
+
+std::vector<int> identifySinks(std::vector<std::vector<int>> tree, std::vector<int> layerList)
+{
+    std::vector<int> ss;
+
+    for (unsigned long int i = 0; i < tree.size(); i++)
+    {
+        bool cf = false;
+
+        for (int v : tree[i])
+        {
+            if (layerList[v] > layerList[i])
+            {
+                cf = true;
+                break;
+            }
+        }
+
+        if (!cf)
+        {
+            ss.push_back(i);
+        }
+    }
+
+    return ss;
+}
+
+int countSinks(int start, std::vector<std::vector<int>> sources, std::vector<int> sinks)
+{
+    int sc = 0;
+
+    for (int sink : sinks)
+    {
+        if (start == sink)
+        {
+            return 1;
+        }
+    }
+
+    for (int sink : sinks)
+    {
+        for (int src : sources[sink])
+        {
+            if (start == src)
+            {
+                sc++;
+                break;
+            }
+        }
+    }
+
+    return sc;
 }
 
 int main()
@@ -98,6 +144,18 @@ int main()
             tree[v].push_back(u);
         }
 
+        std::vector<int> layerList = calcLayers(tree);
+        
+        int maxLayer = 0;
+        for (int i = 0; i < n; i++)
+        {
+            maxLayer = std::max(maxLayer, layerList[i]);
+        }
+
+        std::vector<std::vector<int>> layersMap = compileLMap(maxLayer, layerList, tree);        
+        std::vector<std::vector<int>> sources = lookupSrcs(maxLayer, layerList, tree, layersMap);
+        std::vector<int> sinks = identifySinks(tree, layerList);
+
         int q; std::cin >> q;
 
         std::vector<std::vector<int>> al(q);
@@ -108,8 +166,6 @@ int main()
             al[i].push_back(x - 1);
             al[i].push_back(y - 1);
         }
-
-        std::vector<int> d = calcDepths(tree);
         
         std::vector<int> cache(n, 0);
 
@@ -122,7 +178,7 @@ int main()
             }
             else
             {
-                sn0 = countSinks(tree, d, al[i][0]);
+                sn0 = countSinks(al[i][0], sources, sinks);
                 cache[al[i][0]] = sn0;
             }
 
@@ -133,7 +189,7 @@ int main()
             }
             else
             {
-                sn1 = countSinks(tree, d, al[i][1]);
+                sn1 = countSinks(al[i][1], sources, sinks);
                 cache[al[i][1]] = sn1;
             }
 
